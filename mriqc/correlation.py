38b8b0b1930b4793d1d2904e14b9cae6bb5cc1f8
Şeyma Bayrak <sheymaba@gmail.com>
2016-10-19 22:16:14 +0200

    change wm input from apar+aseg to wmseg for single run

--- mriqc/correlation.py
+++ mriqc/correlation.py
@@ -77,27 +77,27 @@ def plot_epi_T1_corregistration(mean_epi_file, wm_file, reg_file, fssubjects_dir
     print "YESSS"
     return fig
 
-subject_id     = sys.argv[1]
-print "OOOOOOOOOOOO", subject_id
-
-# simple run
-infiles = ['/nobackup/ilz2/bayrak/subjects/hc01_d00/preprocessed/func/coregister/rest2anat.dat.mincost',
-           '/nobackup/ilz2/bayrak/subjects/hc02_d00/preprocessed/func/coregister/rest2anat.dat.mincost']
-similarities = get_similarity_distribution(infiles)
-
-print similarities
-
-mean_epi_file  = '/nobackup/ilz2/bayrak/subjects/%s/preprocessed/func/realign/mean_corr_rest_roi.nii.gz'%(subject_id)
-
-reg_file       = '/nobackup/ilz2/bayrak/subjects/%s/preprocessed/func/coregister/transforms2anat/rest2anat.dat'%(subject_id)
-
-fssubjects_dir = '/nobackup/ilz2/bayrak/freesurfer'
 
 
-wm_file = '/nobackup/ilz2/bayrak/subjects/%s/preprocessed/func/denoise/mask/aparc_aseg.nii.gz'%(subject_id)
+## simple run
+#subject_id = 'hc02_d00'
 
-Figure = plot_epi_T1_corregistration(mean_epi_file,  wm_file, reg_file, fssubjects_dir, subject_id, 
-					similarity_distribution=None, figsize=(11.7,8.3))
+#infiles = ['/nobackup/ilz2/bayrak/subjects/hc01_d00/preprocessed/func/coregister/rest2anat.dat.mincost',
+#           '/nobackup/ilz2/bayrak/subjects/hc02_d00/preprocessed/func/coregister/rest2anat.dat.mincost']
+#similarities = get_similarity_distribution(infiles)
 
-plt.show()
-#Figure.savefig('D.pdf', format='pdf')
+#print similarities
+
+#mean_epi_file  = '/nobackup/ilz2/bayrak/subjects/%s/preprocessed/func/realign/mean_corr_rest_roi.nii.gz'%(subject_id)
+
+#reg_file       = '/nobackup/ilz2/bayrak/subjects/%s/preprocessed/func/coregister/transforms2anat/rest2anat.dat'%(subject_id)
+
+#fssubjects_dir = '/nobackup/ilz2/bayrak/freesurfer'
+
+#wm_file = '/nobackup/ilz2/bayrak/subjects/%s/preprocessed/anat/brain_wmedge.nii.gz'%(subject_id)
+
+#Figure = plot_epi_T1_corregistration(mean_epi_file,  wm_file, reg_file, fssubjects_dir, subject_id, 
+#					similarity_distribution=None, figsize=(11.7,8.3))
+#
+#plt.show()
+#Figure.savefig('A_%s.pdf'%(subject_id), format='pdf')

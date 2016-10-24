f5b63bbfb35d90349659299f7f6625d58feadfe3
Şeyma Bayrak <sheymaba@gmail.com>
2016-10-24 20:36:21 +0200

    remove reg_file input with mean_to_anat, add the right wm_file for single run

--- mriqc/reports.py
+++ mriqc/reports.py
@@ -1,5 +1,5 @@
 def create_report(subject_id, tsnr_file, realignment_parameters_file, 
-		  mean_epi_file, wm_file, mask_file, reg_file, 
+		  mean_epi_file, wm_file, mask_file, mean_epi_to_anat, 
 		  fssubjects_dir, similarity_distribution, 
  		  mean_FD_distribution, tsnr_distributions, output_file):
 
@@ -38,10 +38,11 @@ def create_report(subject_id, tsnr_file, realignment_parameters_file,
     report.savefig(fig, dpi=300)
     fig.clf()
     plt.close()
-        
-    fig = plot_epi_T1_corregistration(mean_epi_file, wm_file, 
-                                      reg_file, fssubjects_dir, subject_id, 
-                                      similarity_distribution, figsize=(8.3, 8.3))
+    
+    fig = plot_epi_T1_corregistration(mean_epi_to_anat, wm_file, 
+				      fssubjects_dir, subject_id, 
+				      similarity_distribution, figsize=(8.3,8.3))
+    
     report.savefig(fig, dpi=300)
     fig.clf()
     plt.close()
@@ -51,7 +52,6 @@ def create_report(subject_id, tsnr_file, realignment_parameters_file,
     plt.close()
     
     return output_file, subject_id
-
 
 
 def read_dists(csv_file):
@@ -75,22 +75,21 @@ def check(subject_id, scan_id, checklist):
 ## single run
 #data_dir   = "/nobackup/ilz2/bayrak/subjects/"
 
-#subject_id                  = 'hc01_d00'
+#subject_id                  = 'hc02_d00'
 #tsnr_file  		    = data_dir + "%s/preprocessed/func/realign/corr_rest_roi_tsnr.nii.gz"%(subject_id) 
 #realignment_parameters_file = data_dir + "%s/preprocessed/func/realign/rest_roi.nii.gz.par"%(subject_id)
 #mean_epi_file 		    = data_dir + "%s/preprocessed/func/realign/mean_corr_rest_roi.nii.gz"%(subject_id)
-#wm_file 		    = data_dir + "%s/preprocessed/func/denoise/mask/aparc_aseg.nii.gz"%(subject_id)
+#wm_file                     = data_dir + "%s/preprocessed/anat/brain_wmedge.nii.gz"%(subject_id)
 #mask_file		    = data_dir + "%s/preprocessed/func/denoise/mask/brain_mask_func.nii.gz"%(subject_id)
-#reg_file 		    = data_dir + "%s/preprocessed/func/coregister/transforms2anat/rest2anat.dat"%(subject_id)
+#mean_epi_to_anat            = data_dir + "%s/preprocessed/func/coregister/rest2anat_highRes.nii.gz"%(subject_id)
 #fssubjects_dir 		    = "/nobackup/ilz2/bayrak/freesurfer/"
 #similarity_distribution     = {'hc01_d00': 0.45303, 'hc02_d00' : 0.4000}
 #mean_FD_distribution        = [0.07, 0.08]
 #tsnr_distributions	    = [65, 79]
-#output_file                 = "bla.pdf"
+#output_file                 = "bla2.pdf"
 
 #create_report(subject_id, tsnr_file, realignment_parameters_file, 
-#              mean_epi_file, wm_file, mask_file, reg_file, 
+#              mean_epi_file, wm_file, mask_file, mean_epi_to_anat,
 #	      fssubjects_dir, similarity_distribution, 
 #              mean_FD_distribution, tsnr_distributions, output_file)
-
 

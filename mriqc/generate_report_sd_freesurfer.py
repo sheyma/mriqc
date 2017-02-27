d0599e5d0267c4fe75965924a2b84561a61af1d9
Şeyma Bayrak <sheymaba@gmail.com>
2017-02-27 14:30:15 +0100

    separate freesurfer and rest quality controls

---
+++ mriqc/generate_report_sd_freesurfer.py
@@ -0,0 +1,88 @@
+from glob import glob
+from correlation import get_similarity_distribution
+from motion import get_mean_frame_displacement_disttribution
+from volumes import get_median_distribution
+from reports import create_report
+import pandas as pd
+from nipype.pipeline.engine import Workflow, Node
+from nipype.interfaces.utility import Function
+
+
+if __name__ == '__main__':
+    data_dir       = "/data/pt_mar006/subjects/"
+    out_dir        = "/data/pt_mar006/mriqc/sd_freesurfer/"
+    freesurfer_dir = "/data/pt_mar006/freesurfer/"
+    
+    wf = Workflow("reports")
+    wf.base_dir = "/data/pt_mar006/mriqc_workdir"
+    with open('/data/pt_mar006/documents/all_sd_freesurfer.txt', 'r') as f:
+        subjects = [line.strip() for line in f]
+    
+    subjects.sort()
+
+    #generating distributions
+    mincost_files = [data_dir + "%s/preprocessed/func/coregister/transforms2anat/rest2anat.dat.mincost"%(subject) for subject in subjects]
+    similarity_distribution = get_similarity_distribution(mincost_files)
+    
+      
+    realignment_parameters_files = [data_dir + "%s/preprocessed/func/realign/rest_roi.nii.gz.par"%(subject) for subject in subjects]
+    mean_FD_distribution, max_FD_distribution = get_mean_frame_displacement_disttribution(realignment_parameters_files)
+    
+    tsnr_files = [data_dir + "%s/preprocessed/func/realign/corr_rest_roi_tsnr.nii.gz"%(subject) for subject in subjects]
+    mask_files = [data_dir + "%s/preprocessed/func/denoise/mask/brain_mask_func.nii.gz"%(subject) for subject in subjects]
+    tsnr_distributions = get_median_distribution(tsnr_files, mask_files)
+    
+    df = pd.DataFrame(zip(subjects, similarity_distribution, mean_FD_distribution, 
+			  max_FD_distribution, tsnr_distributions), columns = ["subject_id", 
+                          "coregistration quality", "Mean FD", "Max FD", "Median tSNR"])
+    df.to_csv(out_dir+"rest_summary.csv")
+    
+    similarity_distribution = dict(zip(subjects, similarity_distribution))
+
+    for subject_id in subjects:
+	
+        print subject_id
+
+        #setting paths for this subject
+        tsnr_file                   = data_dir + "%s/preprocessed/func/realign/corr_rest_roi_tsnr.nii.gz"%(subject_id) 
+        realignment_parameters_file = data_dir + "%s/preprocessed/func/realign/rest_roi.nii.gz.par"%(subject_id)
+        mean_epi_file               = data_dir + "%s/preprocessed/func/realign/mean_corr_rest_roi.nii.gz"%(subject_id)
+        wm_file 		            = data_dir + "%s/preprocessed/anat/brain_wmedge.nii.gz"%(subject_id)
+        mask_file                   = data_dir + "%s/preprocessed/func/denoise/mask/brain_mask_func.nii.gz"%(subject_id)
+        mean_epi_to_anat            = data_dir + "%s/preprocessed/func/coregister/rest2anat_highRes.nii.gz"%(subject_id) 
+        fssubjects_dir              = "/data/pt_mar006/freesurfer"
+        output_file                 = out_dir + "%s_rest_report.pdf"%(subject_id)
+
+
+        report = Node(Function(input_names=['subject_id', 
+                                            'tsnr_file', 
+                                            'realignment_parameters_file', 
+                                            'mean_epi_file', 
+			                                'wm_file',
+                                            'mask_file', 
+                                            'mean_epi_to_anat', 
+                                            'fssubjects_dir', 
+                                            'similarity_distribution', 
+                                            'mean_FD_distribution', 
+                                            'tsnr_distributions', 
+                                            'output_file'], 
+                                output_names=['out'],
+                                function = create_report), 
+		                        name="report_%s"%(subject_id).replace(".", "_"))
+
+        report.inputs.subject_id                  = subject_id
+        report.inputs.tsnr_file                   = tsnr_file
+        report.inputs.realignment_parameters_file = realignment_parameters_file
+        report.inputs.mean_epi_file               = mean_epi_file
+        report.inputs.wm_file 			          = wm_file
+        report.inputs.mask_file                   = mask_file
+        report.inputs.mean_epi_to_anat            = mean_epi_to_anat
+        report.inputs.fssubjects_dir 		      = fssubjects_dir
+        report.inputs.similarity_distribution     = similarity_distribution
+        report.inputs.mean_FD_distribution        = mean_FD_distribution
+        report.inputs.tsnr_distributions          = tsnr_distributions
+        report.inputs.output_file                 = output_file
+        report.plugin_args	                      ={'override_specs': 'request_memory = 4000'}
+        wf.add_nodes([report])
+#wf.run()	
+wf.run(plugin='MultiProc', plugin_args={'n_procs' : 30})
---
+++ mriqc/reports_only_rest.py
@@ -0,0 +1,83 @@
+def create_report(subject_id, tsnr_file, realignment_parameters_file, 
+		  mean_epi_file, mask_file,   
+ 		  mean_FD_distribution, tsnr_distributions, output_file):
+
+    import gc
+    import pylab as plt
+    from matplotlib.backends.backend_pdf import PdfPages
+    from volumes import plot_mosaic, plot_distrbution_of_values
+    from correlation import plot_epi_T1_corregistration
+    from motion import plot_frame_displacement
+    
+    report = PdfPages(output_file)
+    
+    fig = plot_mosaic(mean_epi_file, title="Mean EPI", figsize=(8.3, 11.7))
+    report.savefig(fig, dpi=300)
+    fig.clf()
+    
+    fig = plot_mosaic(mean_epi_file, "Brain mask", mask_file, figsize=(8.3, 11.7))
+    report.savefig(fig, dpi=600)
+    fig.clf()
+    
+    fig = plot_mosaic(tsnr_file, title="tSNR", figsize=(8.3, 11.7))
+    report.savefig(fig, dpi=300)
+    fig.clf()
+    
+    fig = plot_distrbution_of_values(tsnr_file, mask_file, 
+        "Subject %s tSNR inside the mask" % subject_id, 
+        tsnr_distributions, 
+        "Median tSNR (over all subjects)", 
+        figsize=(8.3, 8.3))
+    report.savefig(fig, dpi=300)
+    fig.clf()
+    plt.close()
+    
+    fig = plot_frame_displacement(realignment_parameters_file, mean_FD_distribution, 
+				  figsize=(8.3, 8.3))
+    report.savefig(fig, dpi=300)
+    fig.clf()
+    plt.close()
+
+    
+    report.close()
+    gc.collect()
+    plt.close()
+    
+    return output_file, subject_id
+
+
+def read_dists(csv_file):
+    
+    import pandas as pd
+    import numpy as np
+    df = pd.read_csv(csv_file, dtype=object)
+    sim = dict(zip(df['subject_id'], list(np.asarray(df['coregistration quality'], dtype='float64'))))
+    mfd = list(np.asarray(df['Mean FD'], dtype='float64'))
+    tsnr = list(np.asarray(df['Median tSNR'], dtype='float64'))
+    
+    return sim, mfd, tsnr
+
+
+def check(subject_id, scan_id, checklist):
+    
+    with open(checklist%(scan_id), 'a') as f:
+        f.write(subject_id+'\n')
+    return checklist
+
+## single run
+#data_dir      = "/data/pt_mar006/subjects/"
+#subject_id    = 'sd01_d00'
+
+#tsnr_file  		    = data_dir + "%s/preprocessed/func/realign/corr_rest_roi_tsnr.nii.gz"%(subject_id) 
+#realignment_parameters_file = data_dir + "%s/preprocessed/func/realign/rest_roi.nii.gz.par"%(subject_id)
+#mean_epi_file 		    = data_dir + "%s/preprocessed/func/realign/mean_corr_rest_roi.nii.gz"%(subject_id)
+#mask_file		    = data_dir + "%s/preprocessed/func/denoise/mask/brain_mask_func.nii.gz"%(subject_id)
+
+#mean_FD_distribution        = [0.0729219829036655, 0.10485059652362243]
+#tsnr_distributions	    = [70.147452331515126, 54.186802020255364]
+#output_file                 = "bal.pdf"
+
+#create_report(subject_id, tsnr_file, realignment_parameters_file, 
+#              mean_epi_file, mask_file, 
+#              mean_FD_distribution, tsnr_distributions, output_file)
+
